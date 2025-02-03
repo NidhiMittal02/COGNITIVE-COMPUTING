4ec5454f44e4d1ed45cc07320b795e39a2584805
Nidhi Mittal <148682769+NidhiMittal02@users.noreply.github.com>
2025-02-03 19:39:56 +0530

    Remove unused files from assignment 2 and clean up code

--- assignment2/Q5.py
+++
@@ -1,5 +0,0 @@
-sample_dict = {"name": "Kelly", "age":25, "salary":8000, "city":"New york"}
-old_key = "city"
-new_key = "location"
-sample_dict[new_key]= sample_dict.pop(old_key)
-print(sample_dict)
